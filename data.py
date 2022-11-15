46c1a84c4822653087dc2ad23ed65205f327f71d
Jacob Thrasher <jthrasher0100@gmail.com>
2022-11-15 18:37:00 -0500

    Comment

--- data.py
+++ data.py
@@ -2,17 +2,19 @@ from torch.utils.data import Dataset
 import torchvision.transforms as T
 
 class BloomData(Dataset):
-    def __init__(self, img_size):
+    def __init__(self, img_size, lang):
         preprocess = T.Compose([
             T.CenterCrop(),
             T.Resize(img_size)
         ])
         
-
+        # Load images here
+        
     def __len__(self):
         return
 
     def __getitem__(self, idx):
+
         return
 
     def load_images(self):
@@ -20,5 +22,3 @@ class BloomData(Dataset):
 
     def load_label_map(self):
         return
-
-        
