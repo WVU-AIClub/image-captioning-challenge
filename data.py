0bb56e0abf17732288efa2bff492f3ee054e8b31
Jacob Thrasher <58013394+j0ngle@users.noreply.github.com>
2022-11-16 14:36:02 -0500

    Vocab and tokenizer

--- data.py
+++ data.py
@@ -1,12 +1,25 @@
 from torch.utils.data import Dataset
 import torchvision.transforms as T
+from datasets import load_dataset
+import spacy
+from collections import Counter
+import re
+import unicodedata
+
 
 class BloomData(Dataset):
     def __init__(self, img_size, lang):
-        preprocess = T.Compose([
-            T.CenterCrop(),
-            T.Resize(img_size)
-        ])
+        assert lang in ['tha', 'kir', 'hau'], f'Expected selected language to be "tha", "kir", or "hau". Got {lang}'
+
+        # preprocess = T.Compose([
+        #     T.CenterCrop(),
+        #     T.Resize(img_size)
+        # ])
+        self.lang = lang
+        self.dataset = load_dataset("sil-ai/bloom-captioning", self.lang, 
+                       use_auth_token=True, download_mode='force_redownload')
+        train_captions = [self.dataset['train'][i]['caption'] for i in range(len(self.dataset['train']))]
+        self.vocab = self.get_vocab(strings=train_captions)
         
         # Load images here
         
@@ -20,5 +33,52 @@ class BloomData(Dataset):
     def load_images(self):
         return
 
-    def load_label_map(self):
-        return
+    def tokenize(self, caption):
+        tokenized = []
+        if self.lang in ['tha', 'kir']:
+            spacy_lang = 'th' if self.lang == 'tha' else 'ky'
+            nlp = spacy.blank(spacy_lang)
+            doc = nlp(caption)
+            for word in doc:
+                w = unicodedata.normalize('NFKD', word.text.lower()).strip()
+                if w in [' ', '', '\n']: continue
+
+                tokenized.append(self.vocab[w])
+        else:
+            caption.replace('\n', ' ').split(' ')
+            for word in caption:
+                w = unicodedata.normalize('NFKD', word.lower()).strip()
+                if w in [' ', '', '\n']: continue
+
+                tokenized.append(self.vocab[w])
+        return tokenized
+
+    def get_vocab(self, strings):
+        vocab = {"<pad>": 0, "<sos>": 1, "<eos>": 2, "<unk>": 3}
+        if self.lang in ['tha', 'kir']:
+            spacy_lang = 'th' if self.lang == 'tha' else 'ky'
+            nlp = spacy.blank(spacy_lang)
+            words = Counter()
+            for doc in nlp.pipe(strings):
+                for word in doc:
+                    w = unicodedata.normalize('NFKD', word.text.lower()).strip()
+                    if w in [' ', '', '\n']: continue
+                    words[w] += 1
+        else:
+            # Spacy does not support huasa, so we improvise
+            words = set()
+            for s in strings:
+                # Remove punctuation
+                s = re.sub(r'[^\w\s]', '', s)
+
+                # Split on space, lowercase, and add to set
+                s = s.replace('\n', ' ').split(' ')
+                for word in s:
+                    w = unicodedata.normalize('NFKD', word.lower()).strip()
+                    if w in [' ', '', '\n']: continue
+                    words.add(w)
+
+        vocab.update({token: i + len(vocab) for i, token in enumerate(words)})
+
+        # To do: Reverse lookup
+        return vocab
--- sandbox.py
+++ sandbox.py
@@ -5,25 +5,43 @@ from utils import plot_multiple_lists
 from datetime import datetime
 import logging
 import torch
+from datasets import load_dataset
+from spacy.vocab import Vocab
+import spacy
+from collections import Counter
+from data import BloomData
 
 
 # pretrained_vit = ViT('B_32', pretrained=True)
-model = Model(i_dim=224,
-                p_dim=16,
-                dim=768,
-                n_heads=4,
-                n_encoder_layers=6,
-                n_decoder_layers=6,
-                dropout=0,
-                device='cuda')
-# model = initialize_model(model, pretrained_vit, n=12)
+# model = Model(i_dim=224,
+#                 p_dim=16,
+#                 dim=768,
+#                 n_heads=4,
+#                 n_encoder_layers=6,
+#                 n_decoder_layers=6,
+#                 dropout=0,
+#                 device='cuda')
+# # model = initialize_model(model, pretrained_vit, n=12)
 
-noise = torch.rand([32, 3, 224, 224])
-caption = torch.randint(high=1000, size=(32, 196))
+# noise = torch.rand([32, 3, 224, 224])
+# caption = torch.randint(high=1000, size=(32, 196))
 
-out = model(noise, caption)
+# out = model(noise, caption)
 
-print(out)
-print(out.size())
+# print(out)
+# print(out.size())
 
+# Add the relevant ISO code for the language you want to work with.
+# iso639_3_letter_code = "hau"
+iso639_3_letter_code = "tha"
+# iso639_3_letter_code = "kir"
+
+dataset = BloomData(img_size=128, lang=iso639_3_letter_code)
+
+train = dataset.dataset['train']
+caption = train[123]['caption']
+
+tokenized = dataset.tokenize(caption)
+print(caption)
+print(tokenized)
 
